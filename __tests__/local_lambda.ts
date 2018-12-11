f99cdf9b1d7effabc5771849f4b42959e2d5d3da
mpppk <niboshiporipori@gmail.com>
2018-12-11 23:23:51 +0900

    Add terminate method to local lambda

--- __tests__/local_lambda.ts
+++ __tests__/local_lambda.ts
@@ -14,132 +14,136 @@ const generateCreateFunctionRequest = <T, U>(name: string, handler: LambdaFuncti
   };
 };
 
-it('return values via callback', async (done) => {
-  const localLambda = new LocalLambda();
+describe('local lambda', () => {
+  let localLambda: LocalLambda;
 
-  await createFunction(localLambda, generateCreateFunctionRequest('hello', (_event, _context, callback) => {
-    callback(null, { message: 'hello world' });
-  }));
-
-  localLambda.invoke({ FunctionName: 'hello' }, (err, result) => {
-    if (err) {
-      fail(err);
-    }
-    if (!result) {
-      fail('result does not returned');
-    }
-    if (result!.FunctionError === 'Handled') {
-      fail('function error is handled: ' + result!.Payload);
-    }
-    if (!result!.Payload) {
-      fail('payload does not exist: ' + result ? JSON.stringify(result!) : '');
-    }
-    const payload = JSON.parse(result!.Payload as string);
-    expect(payload.message).toBe('hello world');
-    done();
+  beforeEach(() => {
+    localLambda = new LocalLambda();
   });
-});
 
-it('return values via return syntax', async (done) => {
-  const localLambda = new LocalLambda();
-  await createFunction(localLambda, generateCreateFunctionRequest('hello', () => {
-    return { message: 'hello world' };
-  }));
-
-  localLambda.invoke({ FunctionName: 'hello'}, (err, result) => {
-    if (err) {
-      fail(err);
-    }
-    if (!result || !result.Payload) {
-      fail('payload does not exist');
-    }
-    const payload = JSON.parse(result!.Payload as string);
-    expect(payload.message).toBe('hello world');
-    done();
+  afterEach(async () => {
+    await localLambda.terminate(true);
   });
-});
 
-it('can handle event payload', async (done) => {
-  const localLambda = new LocalLambda();
-  await createFunction(localLambda, generateCreateFunctionRequest('hello', (event, _context, callback) => {
-    callback(null, { message: (event as any).message });
-  }));
+  it('return values via callback', async (done) => {
+    await createFunction(localLambda, generateCreateFunctionRequest('hello', (_event, _context, callback) => {
+      callback(null, { message: 'hello world' });
+    }));
 
-  const Payload = {message: 'hello'}; // tslint:disable-line
-
-  localLambda.invoke({ FunctionName: 'hello', Payload: JSON.stringify(Payload)}, (err, result) => {
-    if (err) {
-      fail(err);
-    }
-    if (!result || !result.Payload) {
-      fail('payload does not exist');
-    }
-    const newPayload = JSON.parse(result!.Payload as string);
-    expect(newPayload.message).toBe(Payload.message);
-    done();
-  });
-});
-
-it('return error as payload if lambda function return error via callback', async (done) => {
-  const localLambda = new LocalLambda();
-  const errorMessage = 'error for test';
-  await createFunction(localLambda, generateCreateFunctionRequest('hello', (event, _context, callback) => {
-    callback(new Error((event as any).errorMessage), null); // FIXME
-  }));
-
-  localLambda.invoke({ FunctionName: 'hello', Payload: JSON.stringify({errorMessage}) }, (err, result) => {
-    if (err) {
-      fail(err);
-    }
-    if (!result || !result.Payload) {
-      fail('payload does not exist');
-    }
-    expect(result!.FunctionError).toBe('Handled');
-    expect(typeof result!.Payload).toBe('string');
-    try {
+    localLambda.invoke({ FunctionName: 'hello' }, (err, result) => {
+      if (err) {
+        fail(err);
+      }
+      if (!result) {
+        fail('result does not returned');
+      }
+      if (result!.FunctionError === 'Handled') {
+        fail('function error is handled: ' + result!.Payload);
+      }
+      if (!result!.Payload) {
+        fail('payload does not exist: ' + result ? JSON.stringify(result!) : '');
+      }
       const payload = JSON.parse(result!.Payload as string);
-      expect(payload.errorType).toBe('Error');
-      expect(payload.errorMessage).toBe(errorMessage);
+      expect(payload.message).toBe('hello world');
       done();
-    } catch(e) {
-      fail(e);
+    });
+  });
+
+  it('return values via return syntax', async (done) => {
+    await createFunction(localLambda, generateCreateFunctionRequest('hello', () => {
+      return { message: 'hello world' };
+    }));
+
+    localLambda.invoke({ FunctionName: 'hello'}, (err, result) => {
+      if (err) {
+        fail(err);
+      }
+      if (!result || !result.Payload) {
+        fail('payload does not exist');
+      }
+      const payload = JSON.parse(result!.Payload as string);
+      expect(payload.message).toBe('hello world');
       done();
-    }
+    });
+  });
+
+  it('can handle event payload', async (done) => {
+    await createFunction(localLambda, generateCreateFunctionRequest('hello', (event, _context, callback) => {
+      callback(null, { message: (event as any).message });
+    }));
+
+    const Payload = {message: 'hello'}; // tslint:disable-line
+
+    localLambda.invoke({ FunctionName: 'hello', Payload: JSON.stringify(Payload)}, (err, result) => {
+      if (err) {
+        fail(err);
+      }
+      if (!result || !result.Payload) {
+        fail('payload does not exist');
+      }
+      const newPayload = JSON.parse(result!.Payload as string);
+      expect(newPayload.message).toBe(Payload.message);
+      done();
+    });
+  });
+
+  it('return error as payload if lambda function return error via callback', async (done) => {
+    const errorMessage = 'error for test';
+    await createFunction(localLambda, generateCreateFunctionRequest('hello', (event, _context, callback) => {
+      callback(new Error((event as any).errorMessage), null); // FIXME
+    }));
+
+    localLambda.invoke({ FunctionName: 'hello', Payload: JSON.stringify({errorMessage}) }, (err, result) => {
+      if (err) {
+        fail(err);
+      }
+      if (!result || !result.Payload) {
+        fail('payload does not exist');
+      }
+      expect(result!.FunctionError).toBe('Handled');
+      expect(typeof result!.Payload).toBe('string');
+      try {
+        const payload = JSON.parse(result!.Payload as string);
+        expect(payload.errorType).toBe('Error');
+        expect(payload.errorMessage).toBe(errorMessage);
+        done();
+      } catch(e) {
+        fail(e);
+        done();
+      }
+    });
+  });
+
+  it('return error as payload if lambda function reject promise', async (done) => {
+    await createFunction(localLambda, generateCreateFunctionRequest('hello', (event) => {
+      throw new Error((event as any).errorMessage);
+    }));
+    const Payload = {errorMessage: 'error for test'}; // tslint:disable-line
+
+    localLambda.invoke({ FunctionName: 'hello', Payload: JSON.stringify(Payload)}, (err, result) => {
+      if (err) {
+        fail(err);
+      }
+      if (!result || !result.Payload) {
+        fail('payload does not exist');
+      }
+      const newPayload = JSON.parse(result!.Payload as string);
+      expect(newPayload.errorMessage).toBe(Payload.errorMessage);
+      done();
+    });
+  });
+
+  it('return error if invoke nonexistent function', (done) => {
+    localLambda.invoke({ FunctionName: 'nonexistent' }, (err) => {
+      if (!err) {
+        fail('nonexistent function invoking must return ResourceNotFoundException');
+        done();
+      }
+      expect(err!.name).toBe('ResourceNotFoundException');
+      expect(err!.message).toEqual(expect.stringContaining('Function not found: arn:aws:lambda:'));
+      expect(err!.statusCode).toEqual(404);
+      done();
+    });
   });
 });
 
-it('return error as payload if lambda function reject promise', async (done) => {
-  const localLambda = new LocalLambda();
-
-  await createFunction(localLambda, generateCreateFunctionRequest('hello', (event) => {
-    throw new Error((event as any).errorMessage);
-  }));
-  const Payload = {errorMessage: 'error for test'}; // tslint:disable-line
-
-  localLambda.invoke({ FunctionName: 'hello', Payload: JSON.stringify(Payload)}, (err, result) => {
-    if (err) {
-      fail(err);
-    }
-    if (!result || !result.Payload) {
-      fail('payload does not exist');
-    }
-    const newPayload = JSON.parse(result!.Payload as string);
-    expect(newPayload.errorMessage).toBe(Payload.errorMessage);
-    done();
-  });
-});
-
-it('return error if invoke nonexistent function', (done) => {
-  const localLambda = new LocalLambda();
-
-  localLambda.invoke({ FunctionName: 'nonexistent' }, (err) => {
-    if (!err) {
-      fail('nonexistent function invoking must return ResourceNotFoundException');
-      done();
-    }
-    expect(err!.name).toBe('ResourceNotFoundException');
-    expect(err!.message).toEqual(expect.stringContaining('Function not found: arn:aws:lambda:'));
-    expect(err!.statusCode).toEqual(404);
-    done();
-  });
-});
